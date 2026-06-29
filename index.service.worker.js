06000c1ef000db99fad4b0be5253572e3d72ef7b
radpas <pasha.hartedi@gmail.com>
2026-06-29 15:19:50 +0700

    FIANL HOLY

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1782707745|35825394014';
+const CACHE_VERSION = '1782721182|49261529172';
 /** @type {string} */
 const CACHE_PREFIX = 'interface tugas -sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
