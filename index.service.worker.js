894be61e57140fb31dc472f774839bcaf2adc6b5
radpas <pasha.hartedi@gmail.com>
2026-06-29 11:36:22 +0700

    woww

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1781872513|3043556015';
+const CACHE_VERSION = '1782707745|35825394014';
 /** @type {string} */
 const CACHE_PREFIX = 'interface tugas -sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
